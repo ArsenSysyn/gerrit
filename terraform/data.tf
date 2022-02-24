29578575e1bcd1d6c14928e1437c7c88ab9fac35
Arsen <Arsen>
2022-02-24 10:52:42 +0000

    Terraform

---
+++ terraform/data.tf
@@ -0,0 +1,25 @@
+data "aws_iam_policy_document" "ecs_agent" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_instance" "test" {
+  instance_tags = {
+    "Name" = aws_elastic_beanstalk_environment.GerritApp-env.name
+  }
+}
+data "aws_instance" "foo" {
+  instance_tags = {
+    "Name" = "CacheContainer"
+  }
+  depends_on = [
+    aws_autoscaling_group.asg_ecs
+  ]
+
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "env_name" {
+  default = "Gerrit-env"
+}
+
+variable "bucketstate" {
+  default = "mybucketforterraformstatearsen"
+}
+
+variable "dynamodbstate" {
+  default = "dynamodb-lock"
+}
+
