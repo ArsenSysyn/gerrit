1f1adb36cf6c4d8d5a86afd5cb616bf82d5a8393
ArsenSysyn <92683307+ArsenSysyn@users.noreply.github.com>
2022-02-24 13:14:31 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,3 @@
 variable "env_name" {
   default = "Gerrit-env"
 }
-
-variable "bucketstate" {
-  default = "mybucketforterraformstatearsen"
-}
-
-variable "dynamodbstate" {
-  default = "dynamodb-lock"
-}
-
