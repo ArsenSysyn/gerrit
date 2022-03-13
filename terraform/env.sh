f4dae4318b7ab211eb767a489b5c77c31459ecf7
ArsenSysyn <92683307+ArsenSysyn@users.noreply.github.com>
2022-03-13 13:26:52 +0200

    Delete env.sh

--- terraform/env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export IP_CACHE=$(terraform output -raw cacheip)
-export APP_NAME=$(terraform output -raw appname)
-export APP_ENV_NAME=$(terraform output -raw envname)
-export IP_APP=$(terraform output -raw eip)
-
