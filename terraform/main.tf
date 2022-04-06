fa6bfcb3491cd2758afbef3a45efd70cfb6627bf
ArsenSysyn <92683307+ArsenSysyn@users.noreply.github.com>
2022-04-06 13:57:42 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
  terraform {
    backend "s3" {
-//     bucket         = "env-variable"
+     bucket         = "mybucketforterraformstatearsen"
      key            = "first-test-2/terraform.tfstate"
      region         = "us-east-1"
-//     dynamodb_table = "env-variable"
+     dynamodb_table = "dynamodb-lock"
      encrypt        = true
    }
 }
